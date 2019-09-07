6a4e64e805c0484e0b15be9558af234f6d59a494
Christian <code@quantentunnel.de>
2019-09-07 02:03:46 +0200

    first commit

---
+++ canvas.js
@@ -0,0 +1,200 @@
+"use strict";
+
+function Point(x,y){
+	this.x=x;
+	this.y=y;
+	this.toString=function(){
+		return "P["+x+","+y+"]";
+	}
+}// end #Point()
+
+let WIDTH=512;
+let HEIGHT=512;
+let points=[];
+
+let kdtree=null;
+
+let selectedPoint={
+	point: null,
+	radius: 10
+};
+
+function init(){
+	// create points
+	points=[];
+	for(let i=0; i<15; i++){
+		let x=Math.random()*WIDTH;
+		let y=Math.random()*HEIGHT;
+		x=Number.parseInt(x);
+		y=Number.parseInt(y);
+		points.push(new Point(x,y));
+	}
+	// sort in tree
+	for(let i=0; i<points.length; i++) treeAdd(points[i]);
+	// draw
+	draw();
+	// events
+	let canvas=document.querySelector("#canvas");
+	document.getElementById("canvas").addEventListener('click', function(e){
+		let r=e.target.getBoundingClientRect();
+		let x=Number.parseInt(e.clientX-r.x);
+		let y=Number.parseInt(e.clientY-r.y);
+		//console.log(`'- x=${x}, y=${y}`);
+		/*let len=points.push(new Point(x,y));
+		treeAdd(points[len-1]); // */
+		treeFindNearest(x,y);
+		selectedPoint.point=new Point(x,y);
+		draw();
+	});
+}// end #init()
+
+// draws a Point object
+function pset(ctx,p){
+	let s=1;
+	ctx.fillRect(p.x-s, p.y-s, 2*s+1, 2*s+1);
+	ctx.beginPath();
+	ctx.moveTo(p.x, p.y-s);
+	ctx.lineTo(p.x, p.y+s);
+	ctx.moveTo(p.x-s, p.y);
+	ctx.lineTo(p.x+s, p.y);
+	ctx.stroke(); // */
+}
+
+
+function draw(){
+	let canvas=document.getElementById("canvas");
+	if(!canvas.getContext) return;
+	let ctx=canvas.getContext("2d");
+	ctx.fillStyle="#fff";
+	ctx.fillRect(0,0,WIDTH, HEIGHT);
+	//ctx.clearRect(0,0,WIDTH, HEIGHT);
+
+	ctx.fillStyle="#000";
+	ctx.strokeStyle="#00f";
+	for(let i=0; i<points.length; i++){
+		pset(ctx, points[i]);
+	}
+	ctx.fillStyle="#f00";
+	ctx.strokeStyle="rgba(0,0,255,0.5)";
+	drawNode(ctx);
+
+	if(selectedPoint.point!==null){
+		ctx.strokeStyle="rgba(255,0,0,0.75)";
+		ctx.beginPath();
+		let p=selectedPoint.point;
+		ctx.arc(p.x, p.y, selectedPoint.radius, 0, 2*Math.PI);
+		ctx.stroke();
+	}
+}// end #draw()
+
+
+function Node(pnt, dim){
+	this.pnt=pnt;
+	this.dim=dim;
+	this.left=null;
+	this.right=null;
+}// end #Node()
+
+
+function treeAdd(pnt, node){
+	if(typeof node==="undefined") node=kdtree;
+	if(kdtree===null){
+		kdtree=new Node(pnt,0);
+		return;
+	}
+
+	if(node.dim==0){// test for x
+		if(pnt.x<=node.pnt.x){
+			if(node.left===null){
+				node.left=new Node(pnt,(node.dim+1)%2);
+			}else treeAdd(pnt, node.left);
+		}else{
+			if(node.right===null) node.right=new Node(pnt, (node.dim+1)%2);
+			else treeAdd(pnt, node.right);
+		}
+		return;
+	}else if(node.dim==1){
+		if(pnt.y<=node.pnt.y){
+			if(node.left===null) node.left=new Node(pnt, (node.dim+1)%2);
+			else treeAdd(pnt, node.left);
+		}else{
+			if(node.right===null) node.right=new Node(pnt, (node.dim+1)%2);
+			else treeAdd(pnt, node.right);
+		}
+		return;
+	}else{
+		console.log("ERROR: node.dim="+node.dim);
+		return;
+	}
+}// end #treeAdd()
+
+
+function drawNode(ctx, node, rect){
+	if(typeof ctx==="undefined") return;
+	if(node===null) return;
+	if(typeof node==="undefined") node=kdtree;
+	if(typeof rect==="undefined") rect=[0,0,WIDTH,HEIGHT];
+
+	if(node.dim==0){
+		ctx.beginPath();
+		ctx.moveTo(node.pnt.x, rect[1]);
+		ctx.lineTo(node.pnt.x, rect[3]);
+		ctx.stroke();
+		if(node.left!==null){
+			let r=rect.slice();
+			r[2]=node.pnt.x;
+			drawNode(ctx, node.left, r);
+		}
+		if(node.right!==null){
+			let r=rect.slice();
+			r[0]=node.pnt.x;
+			drawNode(ctx, node.right, r);
+		}
+	}else{
+		ctx.beginPath();
+		ctx.moveTo(rect[0], node.pnt.y);
+		ctx.lineTo(rect[2], node.pnt.y);
+		ctx.stroke();
+		if(node.left!==null){
+			let r=rect.slice();
+			r[3]=node.pnt.y;
+			drawNode(ctx, node.left, r);
+		}
+		if(node.right!==null){
+			let r=rect.slice();
+			r[1]=node.pnt.y;
+			drawNode(ctx, node.right, r);
+		}
+	}
+
+	
+}// end #drawNode()
+
+
+function treeFindNearest(x,y,node,nearest){
+	if(typeof node==="undefined") node=kdtree;
+	if(node===null) return;
+	if(typeof nearest==="undefined") nearest=Math.sqrt(WIDTH*WIDTH, HEIGHT*HEIGHT);
+
+	let dx=Math.abs(x-node.pnt.x);
+	let dy=Math.abs(y-node.pnt.y)
+
+	let dist=Math.pow(x-node.pnt.x,2)+Math.pow(y-node.pnt.y,2);
+	dist=Math.sqrt(dist);
+
+	if(dist<nearest){
+		nearest=dist;
+		selectedPoint.point=node.pnt;
+		selectedPoint.radius=nearest;
+	}
+
+	if(node.dim==0){
+		if(x>node.pnt.x) treeFindNearest(x,y, node.right, nearest);
+		else treeFindNearest(x,y, node.left, nearest);
+	}else{
+		if(y>node.pnt.y) treeFindNearest(x,y, node.right, nearest);
+		else treeFindNearest(x,y, node.left, nearest);
+	}
+
+}// end treeFindNearest()
+
